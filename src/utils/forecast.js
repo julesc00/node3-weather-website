a44a83b9559fd590d50eeb9f5b5e9118b9bd0ef2
julesc00 <35859040+julesc00@users.noreply.github.com>
2019-07-08 18:14:47 -0500

    Adding extra data to show: High & Low temperatures.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' Currently it is: ' + body.currently.temperature + '°. There is a ' + body.currently.precipProbability + '% of rain.')
+            
+            callback(undefined, body.daily.data[0].summary + ' Currently it is: ' + body.currently.temperature + '°. The high today is ' + body.daily.data[0].temperatureHigh + '. The lowest is '+ body.daily.data[0].temperatureLow + '.' + ' There is a ' + body.currently.precipProbability + '% of rain.')
         }
     })
 }
